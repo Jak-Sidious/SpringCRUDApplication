783d3f699601cc6524d084445db98fd602f1cf30
Jakana Kiwanuka <k.jak86@yahoo.com>
2020-02-18 15:20:33 +0300

    Implement Logging with Log4j2

--- src/main/java/com/example/SpringCrud/SpringCrudApplication.java
+++ src/main/java/com/example/SpringCrud/SpringCrudApplication.java
@@ -1,12 +1,16 @@
 package com.example.SpringCrud;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCrudApplication {
 
+    private static final Logger logger = LogManager.getLogger(SpringCrudApplication.class);
     public static void main(String[] args) {
+        logger.info("Starting the Spring boot application");
         SpringApplication.run(SpringCrudApplication.class, args);
     }
 
