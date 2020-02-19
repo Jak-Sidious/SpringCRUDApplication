7f5b8befc71739845f32a6ac962f0ac7bd2430dd
Jakana Kiwanuka <k.jak86@yahoo.com>
2020-02-19 17:11:41 +0300

    Implement Get all and create Book methods

--- src/main/java/com/example/SpringCrud/controller/BookController.java
+++ src/main/java/com/example/SpringCrud/controller/BookController.java
@@ -1,7 +1,32 @@
 package com.example.SpringCrud.controller;
 
+import com.example.SpringCrud.model.Book;
+import com.example.SpringCrud.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
+
+    @Autowired
+    private  BookService bookService;
+
+    BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping("/books")
+    Book createBook(@RequestBody Book newBook){
+        return bookService.createBook(newBook);
+    }
 }
