0a406b19721d01c0bf4b45a003c3d1c85f8e9e5b
Jak-Sidious <30991429+Jak-Sidious@users.noreply.github.com>
2020-02-24 16:08:10 +0300

    Ft #171377263 user entity (#5)
    * Implement the User Entity and all requisite endpoints

---
+++ src/main/java/com/example/SpringCrud/service/iUserService.java
@@ -0,0 +1,17 @@
+package com.example.SpringCrud.service;
+
+import com.example.SpringCrud.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+interface IUserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User createUser(User newUser);
+    Object updateUser(Long id, User updatedUser);
+    User deleteUser(Long id);
+    User borrowBook(Long userId, Long bookId);
+    User returnBook(Long userId, Long bookId);
+}
